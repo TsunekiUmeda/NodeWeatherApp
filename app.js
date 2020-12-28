30d3aacb29342bbbf810e140eff41c40e7fce3f9
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-28 20:09:07 +0900

    Add geolocation service

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const request = require('postman-request')
-const api = require('./api')
-const url = `http://api.weatherstack.com/current?access_key=${api.access_key}&query=37.8267,-122.4233`
+const { mapBox_access_key, weather_access_key } = require('./api')
+const url = `http://api.weatherstack.com/current?access_key=${weather_access_key}&query=37.8267,-122.4233`
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(response.body.current)
+  const temp = response.body.current.temperature
+  const feelsLike = response.body.current.feelslike
+  const description = response.body.current.weather_descriptions[0]
+  console.log(
+    `${description}. It is currently ${temp} degrees out. It feels like ${feelsLike} degrees out`
+  )
+})
+
+const geo_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapBox_access_key}&limit=1`
+
+request({ url: geo_url, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1]
+  const longitude = response.body.features[0].center[0]
 })
