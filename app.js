5f94670750504a3d29bd18bac428289aea7edefd
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-29 19:59:03 +0900

    Callback chaining

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Philadelphia New York', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+const address = process.argv[2]
+if (!address) {
+  return console.log('Please provide an address')
+}
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
+geocode(address, (error, data) => {
+  if (error) {
+    return console.log(error)
+  }
+
+  forecast(data.longitude, data.latitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log(data.location)
+    console.log(forecastData)
+  })
 })
